fb2e423b857e17e5a2a044eb3289ca62f1aa47f4
朱海龙 <2300305359@qq.com>
2020-11-09 17:46:17 +0800

    TestGit添加sayHello功能

--- test_git/src/git/TestGit.java
+++ test_git/src/git/TestGit.java
@@ -4,4 +4,8 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
     }
+
+    public void sayHello(){
+        System.out.println("Hello World!!!");
+    }
 }
